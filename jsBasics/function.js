2a0553eb890f219b53fdecbcc196024f16b75b41
Godswillben <amogugodswill99@gmail.com>
2023-05-13 22:50:12 +0100

    3d basics

---
+++ jsBasics/function.js
@@ -0,0 +1,15 @@
+function multiply(a, b){
+    return a * b;
+}
+multiply(2, 3)
+multiply(12, 7)
+
+function degToRads(angle){
+    const pi = 3.1416;
+    return angle / 180 * pi;
+}
+
+function milesToKm(dist){
+    return dist * 1.609;
+}
+
